db2373c5d84c4ec1854a2335ed5d7a4ea31753f7
Lucas Ciziks <luciziks@hotmail.com>
2023-11-23 14:52:53 -0300

    Adding Hate Speech Model

--- sentiment.py
+++ sentiment.py
@@ -7,19 +7,31 @@ from transformers import (
     pipeline,
 )
 
+tokenizer_h = BertTokenizer.from_pretrained("Guspfc/my-awesome-bert-model-hate-speech")
+model_h = TFAutoModelForSequenceClassification.from_pretrained(
+    "Guspfc/my-awesome-bert-model-hate-speech"
+)
+
+
 class SentimentAnalyzer:
-    bert_model: str = "Guspfc/my-awesome-bert-model-sentiment-analysis"
+    sentiment_model: str = "Guspfc/my-awesome-bert-model-sentiment-analysis"
+    hate_speech_model: str = "Guspfc/my-awesome-bert-model-hate-speech"
 
     # Initialize Bert Model and Tokenizer
     def __init__(self) -> None:
-        self.tokenizer = BertTokenizer.from_pretrained(self.bert_model)
-        self.model = TFAutoModelForSequenceClassification.from_pretrained(
-            self.bert_model
+        self.tokenizer_sentiment = BertTokenizer.from_pretrained(self.sentiment_model)
+        self.model_sentiment = TFAutoModelForSequenceClassification.from_pretrained(
+            self.sentiment_model
+        )
+
+        self.tokenizer_hate = BertTokenizer.from_pretrained(self.hate_speech_model)
+        self.model_hate = TFAutoModelForSequenceClassification.from_pretrained(
+            self.hate_speech_model
         )
 
     # Method to Analyse sentiment of text
-    def analyze(self, text: str):
-        input_token = self.tokenizer(text, return_tensors="tf")
+    def analyze_sentiment(self, text: str):
+        input_token = self.tokenizer_sentiment(text, return_tensors="tf")
 
         outputs = self.model(**input_token)
         logits = outputs.logits
@@ -34,3 +46,21 @@ class SentimentAnalyzer:
             return "Negative ❌"
         else:
             return "Positive ✅"
+
+    # Method to Analyse Hate Speech in text
+    def analyze_hate_speech(self, text: str):
+        input_token = self.tokenizer_hate(text, return_tensors="tf")
+
+        outputs = self.model(**input_token)
+        logits = outputs.logits
+
+        probabilities = tf.nn.softmax(logits, axis=-1)
+
+        predicted_class = tf.argmax(probabilities, axis=1).numpy()[0]
+
+        if predicted_class == 0:
+            return "Hate Speech Detected ☠️"
+        elif predicted_class == 1:
+            return "Offensive Language Detected 🤬"
+        else:
+            return None
